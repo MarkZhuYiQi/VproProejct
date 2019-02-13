8dee53723701dfd9713357ed5db767eb0f1e359e
MarkZhuYiQi <zhuyiqi_026@163.com>
2019-02-13 19:56:31 +0800

    temp

--- src/store/modules/CategoryModule.js
+++ src/store/modules/CategoryModule.js
@@ -26,13 +26,34 @@ export default{
       const loading = Loading.service({
         text: '正在加载中'
       })
+      /**
+       "total": 302,
+       "list": [xx,xx,xx],
+       "pageNum": 1,
+       "pageSize": 40,
+       "size": 40,
+       "startRow": 1,
+       "endRow": 40,
+       "pages": 8,
+       "prePage": 0,
+       "nextPage": 2,
+       "isFirstPage": true,
+       "isLastPage": false,
+       "hasPreviousPage": false,
+       "hasNextPage": true,
+       "navigatePages": 8,
+       "navigatepageNums": [1-8],
+       "navigateFirstPage": 1,
+       "navigateLastPage": 8
+       */
       return new Promise((resolve, reject) => {
         console.log(context.state.categoryCrumb)
         if (context.state.categoryCrumb.length === 0) return
         loadCategory({ c: context.state.categoryCrumb[context.state.categoryCrumb.length - 1]['navId'], p: page }).then((res) => {
           loading.close()
-          context.commit('SET_COURSESLIST', res.data)
-          resolve(res.data)
+          context.commit('SET_COURSESLIST', res.data.list)
+          context.commit('SET_PAGINATION', res.data.pages)
+          resolve(res.data.list)
         }).catch(err => {
           loading.close()
           reject(err)
